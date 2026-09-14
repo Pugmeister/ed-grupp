39133e9d586e24f271a6f073b31747bf42ed9b01
pugmeiister <artyom_melnikov@icloud.com>
2026-09-14 21:50:09 +0300

    Поменял фотку для промышленного направления

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -246,7 +246,7 @@ export default function Home() {
               data-speed="0.1"
             >
               <img
-                src={img('images/warehouse.jpg')}
+                src={img('images/projects/azerbaijan-complex/01.jpg')}
                 className="w-full h-full object-cover grayscale hover:grayscale-0 transition-all duration-700"
                 alt="Промышленное строительство"
               />
